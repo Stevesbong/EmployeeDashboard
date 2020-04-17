a771d98f0cd724412c5128793b859c2c773a2bc1
Steve Park <ghkfkd0323@gmail.com>
2020-04-17 00:40:06 -0700

    chart.js done

--- js/app.js
+++ js/app.js
@@ -20,13 +20,14 @@ alertBanner.addEventListener('click', e => {
     if( element.classList.contains("alert-banner-close") ) {
         alertBanner.style.display = "none";
     }
-})
+} );
+
 
 /* ============================================= */
 /*                 Traffic Chart                 */
 /* ============================================= */
 
-const trafficCanvas = document.getElementById("traffic-cahrt");
+const trafficCanvas = document.getElementById("traffic-chart").getContext('2d');
 
 let trafficData = {
     labels: [ "16-22", "23-29", "30-5", "6-12", "13-19", 
@@ -38,6 +39,190 @@ let trafficData = {
             1250, 1850, 2250, 1500, 2500 ],
         backgroundColor: "rgba( 116, 119, 191, .3)",
         borderWidth: 1,
+        pointBackgroundColor: "rgba( 255, 255, 255, 1 )",
+        pointBorderColor: "rgba( 116, 119, 191, 1)",
         
     } ]
 }
+
+let trafficOptions = {
+    aspectRatio: 2.5,
+    animation: {
+        duration: 1000,
+        easing: 'easeInOutSine'
+    },
+    scales: {
+        
+        // Y AXES FONT SIZE
+        yAxes: [ {
+            ticks: {
+                beginAtZero: true,
+
+            }
+        }],
+
+        // X AXES FONT SIZE
+        xAxes: [ {
+            ticks: {
+
+            }
+        }]
+    },
+    legend: {
+        display: false,
+    },
+    elements: {
+        line: {
+            tension: 0
+        },
+        point: {
+            radius: 7,
+            hoverRadius: 10
+        }
+    }
+};
+
+let trafficChart = new Chart( trafficCanvas, {
+    type: "line",
+    data: trafficData,
+    options: trafficOptions
+} );
+
+
+
+/* ============================================= */
+/*                 Daily Chart                   */
+/* ============================================= */
+
+const dailyCanvas = document.getElementById("daily-chart");
+
+// DATA FOR DAILY TRAFFIC BAR CHART
+const dailyData = {
+    labels: [ "S", "M", "T", "W", "T", "F", "S" ],
+    datasets: [ {
+        label: "# of Hits",
+        data: [ 75, 115, 175, 125, 225, 200, 100 ],
+        backgroundColor: "#7477BF",
+        borderWidth: 1
+    } ]
+};
+
+const dailyOptions = {
+    scales: {
+        yAxes: [ {
+            ticks: {
+                beginAtZero: true
+            }
+        } ]
+    },
+    legend: {
+        display: false
+    }
+}
+
+let dailyChart = new Chart( dailyCanvas, {
+    type: "bar",
+    data: dailyData,
+    options: dailyOptions
+} );
+
+
+/* ============================================= */
+/*                 Mobile Chart                  */
+/* ============================================= */
+
+const mobileCanvas = document.getElementById("mobile-chart");
+
+const mobileData = {
+    labels: [ "Phones", "Tablet", "Desktop" ],
+    datasets: [ {
+        label: "# of Users",
+        data: [ 550, 500, 2000 ],
+        borderWidth: 0,
+        backgroundColor: [
+            "#51B6C8",
+            "#78CF82",
+            "#7477BF",
+        ]
+    } ]
+};
+
+const mobileOptions = {
+    legend: {
+        position: "right",
+        labels: {
+            boxWidth: 20,
+            fontStyle: "bold"
+        }
+    }
+}
+
+let mobileChart = new Chart(mobileCanvas, {
+    type: "doughnut",
+    data: mobileData,
+    options: mobileOptions
+} );
+
+
+/* ============================================= */
+/*                 Messaging Section             */
+/* ============================================= */
+
+const user = document.getElementById("userfield");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
+
+send.addEventListener('click', () => {
+
+    if ( user.value === "" && message.value === "" ) {
+        alert("Please fill out user and message fields before sending");
+    } else if ( user.value === "" ) {
+        alert("Please fill out user field before sending");
+    } else if (message.value === "" ) {
+        alert("Please fill out message field before sending");
+    } else {
+        alert(`Message successfully sent to: ${user.value}`);
+    }
+} );
+
+
+/* ============================================= */
+/*                 Messaging Section             */
+/* ============================================= */
+
+const xs = window.matchMedia( "(min-width: 320px)" );
+const med = window.matchMedia( "(min-width: 768px)" );
+const lg = window.matchMedia( "(min-width: 1024px)" );
+window.addEventListener('resize', function widthChange() {
+    if (xs.matches) {
+        console.log('xs')
+        trafficChart.options.scales.xAxes[0].ticks.fontSize = 8
+        trafficChart.options.scales.yAxes[0].ticks.fontSize = 8
+        console.log(trafficChart.options.scales.xAxes[0].ticks.fontSize)
+    } 
+    if (med.matches) {
+        console.log('med')
+        trafficChart.options.scales.xAxes[0].ticks.fontSize = 12
+        trafficChart.options.scales.yAxes[0].ticks.fontSize = 12
+        console.log(trafficChart.options.scales.xAxes[0].ticks.fontSize)
+    } 
+    if (lg.matches) {
+        console.log('lg')
+        trafficChart.options.scales.xAxes[0].ticks.fontSize = 16
+        trafficChart.options.scales.yAxes[0].ticks.fontSize = 16
+        console.log(trafficChart.options.scales.xAxes[0].ticks.fontSize)
+    }
+});
+
+
+// if(lg.matches) {
+//     trafficChart.options.scales.xAxes[0].ticks.fontSize = 20
+//     console.log('xsmall')
+
+// } else if (med.matches) {
+//     trafficChart.options.scales.xAxes[0].ticks.fontSize = 12
+//     console.log('medium')
+// } else if (xs.matches) {
+//     trafficChart.options.scales.xAxes[0].ticks.fontSize = 8
+// }
+
